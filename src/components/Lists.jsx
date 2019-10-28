b24dab3ae9d9879ed458fd8db13f31e2ee10edc6
Melissa <yuukohlm@gmail.com>
2019-10-27 19:41:38 -0600

    add new elements to list

---
+++ src/components/Lists.jsx
@@ -0,0 +1,171 @@
+import React  from 'react';
+import styled from 'styled-components';
+
+const ListsContainer = styled.div`
+    font-size: 15px;
+    padding: 2em 1em;
+`;
+
+const ListsH2 = styled.h2`
+    font-weight: 600;
+    letter-spacing: .8px;
+    color: #0a0a3b;
+    text-transform: uppercase;
+    border-bottom: 1px solid #0a0a3b;
+    margin: 1em 0 .5em 0;
+`;
+
+const ListsAdd = styled.div`
+    margin: 0 0 1em 0;
+    .btn {
+        background-color:  #6957ff;
+        cursor: pointer;
+        i {
+            color: #FFF;
+        }
+        &:focus {
+            outline: 0;
+            box-shadow:none;
+        }
+        &:hover {
+            i {
+                color: #00ff94;
+            }
+           
+        }
+    }
+    input {
+        &:focus {
+            outline: 0;
+            box-shadow:none;
+            border-color: #0a0a3b;
+        }
+    }
+`;
+
+const ListsUl = styled.ul`
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+`;
+
+const ListsItem = styled.li`
+    padding: .5em 1em;
+    margin: 1em 0 0 0;
+    font-weight: 600;
+    text-transform: uppercase;
+    background-color:  #0a0a3b;
+    color: #FFF;
+    line-height: 30px;
+    i{
+        float: right;
+        font-size: 30px;
+        cursor: pointer;
+        &:hover {
+            color: #00ff94;
+        }
+    }
+`;
+
+const ListsSubItem = styled.li`
+    padding: .5em 0 .5em 2em;
+    margin: 0;
+    font-weight: 600;
+    color: #6957ff
+`;
+
+ class Lists extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.addNew = this.addNew.bind(this);
+        this.state = { 
+            toDoList: [
+                {
+                    'Study': [
+                        'math',
+                        'english'
+                    ]            
+                },
+                'Laundry',
+                'Call Ming',
+                {
+                    'Read': [
+                        'The name of the wind',
+                        'Throne of Glass',
+                        'Game of thrones'
+                    ]            
+                },
+            ],
+            addNew: '',
+        }
+    }
+
+    renderSubList(items){
+        return items.map( (subItem) => <ListsSubItem key={subItem}>{subItem}</ListsSubItem>)
+    }
+
+    renderListItems() {
+        let mthis =this;
+        return this.state.toDoList.map((function(item) {
+            if(typeof item === 'object') {  
+                var key = Object.keys(item)[0];                              
+                return (
+                    <React.Fragment  key={key}>
+                        <ListsItem>{key} <i onClick={mthis.addSubList} className="fas fa-plus-square"></i></ListsItem>
+                        <ListsUl>
+                            {mthis.renderSubList(item[key])}
+                        </ListsUl> 
+                    </React.Fragment>
+                )              
+                
+            } else {
+                return (
+                    <ListsItem key={item}>{item} <i onClick={mthis.addSubList} className="fas fa-plus-square"></i></ListsItem>
+                );
+            }  
+        }))
+    }
+
+    handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+           this.addNew();
+        }
+    }
+
+    addNew() {
+        if(this.state.addNew) {
+            this.setState({
+                toDoList: [
+                    ...this.state.toDoList,
+                    this.state.addNew
+                ],
+                addNew: '',                 
+            })
+        }
+    }
+
+    addSubList(){
+        alert('otro');
+    }
+
+    render() {
+        return (
+            <ListsContainer className="container">
+                <ListsH2>To do list </ListsH2>
+                <ListsAdd className="input-group">
+                    <input value={this.state.addNew} onKeyDown={this.handleKeyDown} onChange={(e) => this.setState({addNew: e.target.value })} type="text" className="form-control" placeholder="New task"/>
+                    <div className="input-group-append">
+                        <button className="btn" type="button" onClick={this.addNew}><i className="fas fa-plus-square"></i></button>
+                    </div>                    
+                </ListsAdd>
+                
+                <ListsUl>
+                    {this.renderListItems()}
+                </ListsUl>                
+            </ListsContainer>    
+        );
+    }
+ }
+
+export default Lists;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import Lists from '../components/Lists';
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: 'Montserrat', sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #e6e6eb;
+    }    
+`;
 
 const App = () => {
     return (
-        <h1>Hola Mundo</h1>
+        <div>
+            <GlobalStyle/>            
+            <Lists />
+        </div>
     );
 }
 
